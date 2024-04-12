99352210a9e96b3db1667906bf82bf4fc74da087
Minh Ngo <nlminhtl@gmail.com>
2024-04-12 13:23:39 +0200

    Minor fix

--- xlsx2txt.py
+++ xlsx2txt.py
@@ -8,8 +8,7 @@ def main(filename):
     with open(filename, "rb") as f, pl.Config(tbl_cols=-1, tbl_rows=-1):
         df = pl.read_excel(f, sheet_id=0)
         for k, v in df.items():
-            v = v.with_columns([pl.col(column).cast(pl.Utf8) for column in v.columns])
-            v = v.with_columns([pl.col(column).fill_null("") for column in v.columns])
+            v = v.with_columns([pl.col(column).cast(pl.Utf8).fill_null("") for column in v.columns])
 
             print("Table Name:", k)
             print(v)
